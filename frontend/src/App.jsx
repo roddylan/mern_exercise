67264cbcf510cd6ceb2d8a214d864c4a7c35506f
Roderick Lan <rodericklan03@gmail.com>
2024-05-01 20:12:48 -0600

    finished db routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+        <Route path='' element={} />
+
+        <Route path='' element={} />
+        
+        <Route path='' element={} />
+        
+        <Route path='' element={} />
+        
+        <Route path='' element={} />
+    </Routes>
+  )
+}
+
+export default App
